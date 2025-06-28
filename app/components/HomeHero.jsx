ca3a7bf880b11abdc38ce468d60322c2b00057c3
Philipp Isaak <phi.isaak@gmail.com>
2025-06-28 21:39:01 +0200

    stuff

---
+++ app/components/HomeHero.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const HomeHero = ({children}) => {
+    return (
+        <div className="relative w-full h-screen overflow-hidden">
+                <Image
+                    src="/elux-bg.jpg"
+                    alt="Hero"
+                    fill
+                    priority
+                    className="object-cover animated-bg"
+                />
+            <div className="absolute inset-0 bg-white/20 animated-blur mix-blend-overlay z-10" />
+            <div className="absolute inset-0 z-20 animated-gradient bg-linear-to-r from-gray-800 via-blue-700 to-gray-900 bg-200 mix-blend-overlay opacity-65" />
+            <div className="absolute inset-0 z-30 flex items-center justify-center">
+                <div>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomeHero;
